ab78b2c30bae6b05a058a0c44a56c4272821a84d
raza9798 <m.raza9798@gmail.com>
2024-08-22 23:30:33 +0530

    filament integration

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
-require __DIR__.'/auth.php';
